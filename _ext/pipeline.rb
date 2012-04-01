49fe78d1d3ad1c642dfbb0cac9432d0ef40abad2
Paul Dijou <paul.dijou.dev@gmail.com>
2012-04-01 18:08:59 +0200

    Init develop branch

---
+++ _ext/pipeline.rb
@@ -0,0 +1,12 @@
+
+Awestruct::Extensions::Pipeline.new do
+  extension Awestruct::Extensions::Posts.new( '/blog')
+  extension Awestruct::Extensions::Posts.new( '/projects', :projects ) 
+  extension Awestruct::Extensions::Paginator.new( :posts, '/blog/index', :per_page=>1 )
+  extension Awestruct::Extensions::Tagger.new( :posts, '/blog/index', '/blog/tags', :per_page=>10)
+  extension Awestruct::Extensions::TagCloud.new( :posts, '/blog/tags/index.html', :layout=>'main' )
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/blog.atom', :num_entries=>20 )
+  extension Awestruct::Extensions::Disqus.new
+  extension Awestruct::Extensions::Indexifier.new
+  helper Awestruct::Extensions::Partial
+end
---
+++ javascripts/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+	$("[rel=tooltip]").tooltip();
+	$("[rel=tooltip-top]").tooltip({placement: 'top'});
+	$("[rel=tooltip-bottom]").tooltip({placement: 'bottom'});
+	$("[rel=tooltip-left]").tooltip({placement: 'left'});
+	$("[rel=tooltip-right]").tooltip({placement: 'right'});
+	
+	$("a.anchorLink[href^=#]").anchorAnimate();
+	$(".toggleHide").toggleHide();
+	
+	
+	$(function() {
+		var offset = $(".follow-scroll").offset();
+		var speed = 1100;
+		$(window).scroll(function() {
+			if ($(window).scrollTop() > offset.top) {
+				var marginTop = $(window).scrollTop() - offset.top + getTopOffset();
+			} else {
+				var marginTop = 0;
+			};
+			//console.log("Window.scrollTop : "+$(window).scrollTop()+" - offset.top : "+offset.top+" + TopOffset : "+getTopOffset()+" = marginTop : "+marginTop);
+			$(".follow-scroll").stop().animate({marginTop: marginTop}, speed);
+		});
+	});
+});
+
+$.fn.anchorAnimate = function(settings) {
+
+ 	settings = jQuery.extend({
+		speed : 1100
+	}, settings);	
+	
+	return this.each(function(){
+		var caller = this
+		jQuery(caller).click(function (event) {	
+			event.preventDefault()
+			var locationHref = window.location.href
+			var elementClick = jQuery(caller).attr("href")
+			var topOffset = getTopOffset();
+			
+			var destination = jQuery(elementClick).offset().top - getTopOffset();
+			//console.log("Link destination : "+destination + " - topOffset : "+getTopOffset());
+			jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination}, settings.speed);
+		  	return false;
+		})
+	})
+}
+
+$.fn.toggleHide = function() {
+	return this.each(function() {
+		var caller = this
+		jQuery(caller).click(function (event) {	
+			event.preventDefault();
+			var selector = jQuery(caller).attr("data-selector");
+			$(selector).toggleClass("hide");
+		})
+	})
+}
+
+function getTopOffset() {
+	console.log(window.innerWidth);
+	if(window.innerWidth > 979) {
+		return 55;
+	}
+	else {
+		return 5;
+	}
+}
+
